c560e538bef2e41cd934baef7be8f8376df29662
Archives0 <prettyfrills@proton.me>
2025-06-01 02:35:48 -0500

    Hello Triangle

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,34 @@
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 void ProcessInput(GLFWwindow* window);
 
+unsigned int VAO{};
+unsigned int VBO{};
+unsigned int vert{};
+unsigned int frag{};
+unsigned int shaderProgram{};
+
+const char* vertCode = 
+"#version 330 core                      \n"
+"layout (location = 0) in vec3 aPos;    \n"
+"void main()                            \n"
+"{                                      \n"
+"gl_Position = vec4(aPos, 1.0);         \n"
+"}\0";
+
+const char* fragCode =
+"#version 330 core                          \n"
+"out vec4 FragColor;                        \n"
+"void main()                                \n"
+"{                                          \n"
+"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);  \n"
+"}\0";
+
+float vertices[] = {
+    -0.5f, -0.5f, 0,
+    0.5f, -0.5f, 0,
+    0.0f, 0.5f, 0.0f
+};
+
 int main()
 {
     // Initialize glfw.
@@ -32,6 +60,54 @@ int main()
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(mainWindow, framebufferSizeCallback);
 
+    // Create and bind buffers. Sends vertex data to the GPU.
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Set vertex attribute pointers.
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Process shaders.
+    vert = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vert, 1, &vertCode, NULL);
+    glCompileShader(vert);
+
+    int vStatus{};
+    char vLog[512];
+    glGetShaderiv(vert, GL_COMPILE_STATUS, &vStatus);
+    if(!vStatus)
+    {
+        glGetShaderInfoLog(vert, 512, NULL, vLog);
+        std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << vLog <<std::endl;
+    }
+
+    frag = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(frag, 1, &fragCode, NULL);
+    glCompileShader(frag);
+
+    int fStatus{};
+    char fLog[512];
+    glGetShaderiv(frag, GL_COMPILE_STATUS, &fStatus);
+    if(!fStatus)
+    {
+        glGetShaderInfoLog(frag, 512, NULL, fLog);
+        std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << fLog <<std::endl;
+    }
+
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vert);
+    glAttachShader(shaderProgram, frag);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vert);
+    glDeleteShader(frag);
+
     // Render loop.
 
     while(!glfwWindowShouldClose(mainWindow))
@@ -40,6 +116,10 @@ int main()
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(mainWindow);
         glfwPollEvents();
