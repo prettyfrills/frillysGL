f3d8034ccfcd74b26c051025ef1e27ba41920198
Archives0 <prettyfrills@proton.me>
2025-06-02 22:04:18 -0500

    Color shader

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <math.h>
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 void ProcessInput(GLFWwindow* window);
@@ -15,17 +16,21 @@ unsigned int shaderProgram{};
 const char* vertCode = 
 "#version 330 core                      \n"
 "layout (location = 0) in vec3 aPos;    \n"
+"layout (location = 1) in vec3 aColor;  \n"
+"out vec4 vertCol;                      \n"
 "void main()                            \n"
 "{                                      \n"
 "gl_Position = vec4(aPos, 1.0);         \n"
+"vertCol = vec4(aColor, 1.0);           \n"
 "}\0";
 
 const char* fragCode =
 "#version 330 core                          \n"
+"in vec4 vertCol;                           \n"
 "out vec4 FragColor;                        \n"
 "void main()                                \n"
 "{                                          \n"
-"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);  \n"
+"FragColor = vertCol;                       \n"
 "}\0";
 
 float vertices[] = {
@@ -35,14 +40,21 @@ float vertices[] = {
     -0.5f, 0.5f, 0.0f
 };
 
-float bowVerts[] = {
-    -0.5f, -0.5f, 0.0f,
-    -0.5f, 0.5f, 0.0f,
-    0.0f, 0.0f, 0.0f,
+// float bowVerts[] = {
+//     -0.5f, -0.5f, 0.0f,
+//     -0.5f, 0.5f, 0.0f,
+//     0.0f, 0.0f, 0.0f,
 
-    0.0f, 0.0f, 0.0f,
-    0.5f, 0.5f, 0.0f,
-    0.5f, -0.5f, 0.0f
+//     0.0f, 0.0f, 0.0f,
+//     0.5f, 0.5f, 0.0f,
+//     0.5f, -0.5f, 0.0f
+// };
+
+float colorVerts[] = {
+    // position             // color.
+    0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,
+    -0.5f, -0.5f, 0.0f,    0.0f, 1.0f, 0.0f,
+    0.0f, 0.5f, 0.0f,      0.0f, 0.0f, 1.0f
 };
 
 unsigned int indices[] = {
@@ -85,15 +97,18 @@ int main()
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(bowVerts), bowVerts, GL_STATIC_DRAW);
+    // glBufferData(GL_ARRAY_BUFFER, sizeof(bowVerts), bowVerts, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colorVerts), colorVerts, GL_STATIC_DRAW);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Set vertex attribute pointers.
 
     // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     // Process shaders.
     vert = glCreateShader(GL_VERTEX_SHADER);
@@ -141,10 +156,16 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        float time = glfwGetTime();
+        float newCol = (sin(time) / 2.0f) + 0.5f;
+        int colLocation = glGetUniformLocation(shaderProgram, "globalCol");
         glUseProgram(shaderProgram);
+        glUniform4f(colLocation, 0.0f, newCol, 0.0f, 1.0f);
+
         glBindVertexArray(VAO);
-        // glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glDrawArrays(GL_TRIANGLES, 0, 6);
         // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(mainWindow);
