6a77fde9532ae2e9ce2a94555d2fe25e6c5ceba4
prettyfrills <prettyfrills@proton.me>
2025-11-13 23:39:49 -0600

    Improved planet fbm shader

--- src/Shaders/fbmPlanet.glsl
+++ src/Shaders/fbmPlanet.glsl
@@ -35,9 +35,9 @@ uniform vec3 viewPos;
 uniform vec3 lightPos;
 uniform vec3 lightColor;
 
-vec3 ambient = 0.1 * objColor;
+vec3 ambient = 0.0 * objColor;
 
-const int OCTAVES = 2;
+const int OCTAVES = 4;
 
 vec2 random2(vec2 st);
 float gNoise2D(vec2 st);
@@ -63,14 +63,41 @@ void main()
     newCoord.x = texCoord.x * 0.2;
     newCoord += pattern(newCoord);
 
+    // Gradient noise.
     // vec3 noise = vec3(gNoise2D(newCoord * 25));
-    vec3 noise = vec3(pattern(texCoord * 1));
 
-    vec3 col1 = vec3(0.659, 0.486, 0.537);
-    vec3 col2 = vec3(0.612, 0.643, 0.541);
-    vec3 newCol = mix(col1, col2, noise);
+    // FBM noise.
+    // vec3 noise = vec3(pattern(texCoord * 1));
+    vec2 q, r;
+    vec3 noise = vec3(colorPattern(texCoord, q, r));
 
-    FragColor = vec4(newCol, 1.0);
+    // vec3 col1 = vec3(0.659, 0.486, 0.537);
+    // vec3 col2 = vec3(0.612, 0.643, 0.541);
+    // vec3 col3 = vec3(0.686, 0.686, 0.580);
+    // vec3 col4 = vec3(0.753, 0.635, 0.608);
+
+    vec3 col1 = vec3(0.3,0.05,0.05);
+    vec3 col2 = vec3(0.9,0.9,0.9);
+    vec3 col3 = vec3(0.5,0.2,0.2);
+    // vec3 col4 = vec3(0.8, 0.7, 0.7);
+    vec3 col4 = vec3(0.4);
+
+    vec3 color = vec3(0.2,0.1,0.4);
+    color = mix(col1, col2, noise);
+    color = mix(color, col3, dot(r,r));
+    // color = mix(color, col4, 1.5*r.y*r.y);
+    color = mix(color, col4, 0.5*smoothstep(1.2,1.3,abs(r.y)+abs(r.x)));
+
+    // vec3 test = vec3(dot(r, r));
+    // vec3 test = vec3(dot(q, q));
+    vec3 test = vec3(2.0 * q.y * q.y);
+
+    // color *= (noise * 2.0);
+    // color = vec3(1.0) - color;
+    // color /= 1.5;
+    // color = mix(color, col4, 0.5*smoothstep(1.2,1.3,abs(r.y)+abs(r.x)));
+
+    FragColor = vec4(color * light, 1.0);
 }
 
 vec2 random2(vec2 st)
@@ -126,11 +153,13 @@ float pattern(vec2 st)
 
 float colorPattern(vec2 st, out vec2 q, out vec2 r)
 {
-    q.x = fbm( st + vec2(0.0,0.0), 0.5 );
-    q.y = fbm( st + vec2(5.2,1.3), 0.5 );
+    float h = 1.0;
 
-    r.x = fbm( st + 4.0*q + vec2(1.7,9.2), 0.5 );
-    r.y = fbm( st + 4.0*q + vec2(8.3,2.8), 0.5 );
+    q.x = fbm( st + vec2(0.0,0.0), h);
+    q.y = fbm( st + vec2(5.2,1.3), h);
 
-    return fbm( st + 4.0*r, 0.5 );
+    r.x = fbm( st + 4.0*q + vec2(1.7,9.2), h);
+    r.y = fbm( st + 4.0*q + vec2(8.3,2.8), h);
+
+    return fbm( st + 4.0*r, h);
 }
